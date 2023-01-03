85889ee41244ff6cd08ef988fda7d0e3b294396c
Hillary Muller <hillaryaward@gmail.com>
2023-01-03 14:35:51 -0600

    generate treat resources

---
+++ app/serializers/treat_serializer.rb
@@ -0,0 +1,3 @@
+class TreatSerializer < ActiveModel::Serializer
+  attributes :id, :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :treats
   resources :households
   resources :dogs
   resources :users
