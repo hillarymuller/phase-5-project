ba820683d3d10d039d8d1b0cca014fbe8ed899ee
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 14:23:00 -0600

    add error rescuing to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   wrap_parameters format: []
   before_action :authorize
 
@@ -7,6 +10,12 @@ private
 def authorize
   @current_user = User.find_by(id: session[:user_id])
   render json: { error: "Not authorized" }, status: :unauthorized unless @current_user
+end
+def render_unprocessable_entity(invalid)
+  render json: { errors: invalid.record.errors.full_messages }, status: :render_unprocessable_entity
+end
+def render_not_found
+  render json: { error: "Could not find" }, status: :not_found
 end
 
 end
