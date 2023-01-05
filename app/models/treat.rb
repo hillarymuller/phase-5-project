72dc7f74f06b2dea58c507046a7dca3db98eaa51
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 14:32:39 -0600

    add validations

--- app/models/treat.rb
+++ app/models/treat.rb
@@ -1,4 +1,6 @@
 class Treat < ApplicationRecord
     has_many :dog_treats
     has_many :dogs, through: :dog_treats
+    
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   belongs_to :household
   has_many :dogs
+
+  validates :username, :email, presence: true, uniqueness: true, length: {in: 4..20}
+  validates :password, length: {in: 6..20}
+  has_secure_password
 end
