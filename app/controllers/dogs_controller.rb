8bc98de673ee8400d3c1f2fd9cb99d484176a969
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 14:19:49 -0600

    index, show, & update for dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+    skip_before_action :authorize
+
+    def index
+        dogs = Dog.all
+        render json: dogs
+    end
+    def show
+        dog = Dog.find(params[:id])
+        render json: dog
+    end
+    def update
+        dog = Dog.find(params[:id])
+        dog.update!(dog_params)
+        render json: dog
+    end
+
+    private
+    def dog_params
+        params.permit(:id, :user_id, :hunger, :thirst, :happiness, :energy, :potty)
+    end
 end
