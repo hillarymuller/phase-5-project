ed1e4095c461f1e3951171863c03cfbe65b1d9d8
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 13:46:32 -0600

    add create & show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+    def show
+        render json: @current_user
+    end
+
+    private
+    def user_params
+        params.permit(:username, :email, :password, :password_confirmation, :household_id)
+    end
 end
