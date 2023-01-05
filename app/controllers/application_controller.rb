0550d6c64c6ed7ec5774fb543dceda730dbe2d9c
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 13:48:55 -0600

    add authorize before action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  wrap_parameters format: []
+  before_action :authorize
+
+private
+def authorize
+  @current_user = User.find_by(id: session[:user_id])
+  render json: { error: "Not authorized" }, status: :unauthorized unless @current_user
+end
 
 end
