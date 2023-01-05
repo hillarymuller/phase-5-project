e7dafaeafb29dde806b25e0cdb64192e06477890
Hillary Muller <hillaryaward@gmail.com>
2023-01-05 13:39:52 -0600

    add users & sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   resources :treats
   resources :households
   resources :dogs
-  resources :users
+  # user routes
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+
+  # sessions routes
+  post "/signin", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
